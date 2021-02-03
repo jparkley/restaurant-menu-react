b7eadb5419a32a34812e1644579c44e2ba32a257
Jjin SoL <parkleydondon@yahoo.com>
2021-02-02 23:20:09 -0800

    Added build scripts to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require("path")
+const currentTask = process.env.npm_lifecycle_event
 
-module.exports = {
+console.log("task: ", currentTask)
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+const config = {
   entry: "./src/App.js",
+  mode: "development",
   output: {
     publicPath: "/",
     path: path.join(__dirname, "public"),
     filename: "bundled.js"
   },
-  mode: "development",
-  devServer: {
-    port: 3000,
-    contentBase: path.resolve(__dirname, "public"),
-    hot: true,
-    historyApiFallback: { index: "index.html" }
-  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
   module: {
     rules: [
       {
@@ -31,9 +32,47 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        loader: "url-loader"
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
+            }
+          }
+        ]
       }
     ]
   }
 }
+
+if (currentTask == "webpackDev" || currentTask == "dev") {
+  config.devtool = "source-map"
+  config.devServer = {
+    port: 3000,
+    contentBase: path.resolve(__dirname, "public"),
+    hot: true,
+    historyApiFallback: { index: "index.html" }
+  }
+}
+
+if (currentTask == "webpackBuild" || currentTask == "build") {
+  config.mode = "production"
+
+  // Replace "style-loader" with MiniCssExtractPlugin.loader for traditional CSS file inclusion
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+
+  // Add plugins that are needed only for production
+  config.plugins.push(new MiniCssExtractPlugin({ filename: "main.css" }), new CleanWebpackPlugin())
+
+  config.output = {
+    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundled.js",
+    chunkFilename: "[name].[chunkhash].js"
+  }
+}
+
+module.exports = config
